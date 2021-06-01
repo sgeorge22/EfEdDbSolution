b199e73cffb466f5b302b28c36af5f4d6e8d2c11
Sarah George <sarahgeorge1394@gmail.com>
2021-06-01 11:09:27 -0400

    Add project files.

---
+++ EfEdDb/Program.cs
@@ -0,0 +1,31 @@
+﻿using EfEdDb.Models;
+using System;
+using System.Linq;
+
+namespace EfEdDb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var context = new EdDbContext();
+
+            var studentsWithMajors = from s in context.Students
+                                     join m in context.Majors
+                                     on s.MajorId equals m.Id
+                                     where s.StateCode == "KY"
+                                     orderby s.Lastname ascending
+                                     select new { s.Firstname, s.Lastname, Major = m.Description };
+            foreach(var s in studentsWithMajors)
+            {
+                Console.WriteLine($"{s.Firstname} {s.Lastname} || {s.Major}");
+            }
+
+            //var studentsInKy = context.Students.Where(s => s.StateCode == "KY").OrderBy(s => s.Lastname).ToList();
+            //foreach(var s in studentsInKy)
+            //{
+            //    Console.WriteLine($"{s.Firstname} {s.Lastname}");
+            //}
+        }
+    }
+}
